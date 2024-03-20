5a14276588cc8710f69295af9e211b2fc685f979
akinoringo <wbraver@outlook.jp>
2024-03-20 18:12:34 +0900

    feat: Agentが会話履歴を踏まえて応答できるように設定

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
 
 load_dotenv()
 
@@ -16,8 +18,22 @@ def create_agent_chain():
     streaming=True,
   )
 
+  # OpenAI Functions AgentのプロンプトにMemoryの会話履歴を追加する設定
+  agent_kwargs = {
+    "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+  }
+
+  # OpenAI Functions Agentが使える設定でMemoryを初期化
+  memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
   tools = load_tools(["wikipedia"])
-  return initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS)
+  return initialize_agent(
+    tools,
+    chat,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    agent_kwargs=agent_kwargs,
+    memory=memory,
+  )
 
 st.title("sample-app")
 
@@ -30,6 +46,9 @@ for message in st.session_state.messages:
 
 prompt = st.chat_input("こんにちは")
 
+if "agent_chain" not in st.session_state:
+  st.session_state.agent_chain = create_agent_chain()
+
 if prompt:
   st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -38,8 +57,7 @@ if prompt:
 
   with st.chat_message("assistant"):
     callback = StreamlitCallbackHandler(st.container())
-    agent_chain = create_agent_chain()
-    response = agent_chain.run(prompt, callbacks=[callback])
+    response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
     st.markdown(response)
 
   st.session_state.messages.append({"role": "assistant", "content": response})
